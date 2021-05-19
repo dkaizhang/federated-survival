ad05741f3e87ef638a357d66faac627a0a98991b
Kai Zhang <kaizhang@MacBook-Pro.local>
2021-05-19 15:39:14 +0100

    dtype preserving to _csv and read_csv functions

--- load.py
+++ load.py
@@ -158,3 +158,22 @@ def load_table(table_name,
         raise ValueError("The file " + read_path + " does not exist.")
 
     return table
+
+def to_csv(df, path):
+    # Prepend dtypes to the top of df
+    df2 = df.copy()
+    df2.loc[-1] = df2.dtypes
+    df2.index = df2.index + 1
+    df2.sort_index(inplace=True)
+    # Then save it to a csv
+    df2.to_csv(path, index=False)
+
+def read_csv(path):
+    # Read types first line of csv
+    dtypes = {key:value for (key,value) in pd.read_csv(path,    
+              nrows=1).iloc[0].to_dict().items() if 'date' not in value}
+
+    parse_dates = [key for (key,value) in pd.read_csv(path, 
+                   nrows=1).iloc[0].to_dict().items() if 'date' in value]
+    # Read the rest of the lines with the types from above
+    return pd.read_csv(path, dtype=dtypes, parse_dates=parse_dates, skiprows=[1])
