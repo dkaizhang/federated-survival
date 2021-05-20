7f3c7e35871dcfb4becfb36cb26595915616f777
Kai Zhang <kaizhang@MacBook-Pro.local>
2021-05-20 19:16:54 +0100

    Indicator function for determining when a boolean flips

--- utilities.py
+++ utilities.py
@@ -22,7 +22,7 @@ def get_cumulative(table, groupby, date, name, key=None):
     # counts duplicates 
     temp = df.groupby(groupby+[date]).size().reset_index(name='temp2')
 
-    df = pd.merge(df, temp, on=groupby+[date])
+    df = pd.merge(df, temp, on=groupby+[date], how='left')
     
     # take max of duplicate and normal cumulative count 
     df[name] = df[['temp1', 'temp2']].max(axis=1)
@@ -30,3 +30,28 @@ def get_cumulative(table, groupby, date, name, key=None):
     df = df.drop(columns=['temp1', 'temp2'])
     return df
 
+def get_indicator(table, target, groupby, date, name, key=None):
+    df = table.copy()
+
+    if type(groupby) is not list:
+        groupby = [groupby]
+    
+    # collapse the dataframe based on keys
+    if key is not None:
+        if type(key) is not list:
+            key = [key]
+        df = df.drop_duplicates(key)
+
+    # normal cumulative sum hence disregards other values that should share position
+    df['temp1'] = df.sort_values(by=date, ascending=True).groupby(groupby)[target].cumsum()
+    
+    # counts positives 
+    temp = df.loc[df.temp1 > 0].groupby(groupby+[date]).size().reset_index(name='temp2')
+
+    df = pd.merge(df, temp, on=groupby+[date], how='left')
+    
+    # take max of duplicate and normal cumulative count 
+    df[name] = df[['temp1', 'temp2']].max(axis=1) > 0
+    
+    # df = df.drop(columns=['temp1', 'temp2'])
+    return df
