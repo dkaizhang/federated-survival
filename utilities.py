8de96e8bf2a6a31f83fab6f31fa9c7b5763b1641
Kai Zhang <kaizhang@MacBook-Pro.local>
2021-05-20 18:30:25 +0100

    Allow for removal of duplicates; allow lists for key and groupby

--- utilities.py
+++ utilities.py
@@ -1,12 +1,32 @@
 import pandas as pd
 
 # calculates a cumulative count of the target column based on a date column
-def get_cumulative(table, target, date, name):
+# first project only targets and date column
+# collapse to remove duplicates
+
+def get_cumulative(table, groupby, date, name, key=None):
     df = table.copy()
-    df['temp1'] = df.sort_values(by=date, ascending=True).groupby([target]).cumcount() + 1
-    temp = df.groupby([target, date]).size().reset_index(name='temp2')
-    df = pd.merge(df, temp, on=[target, date])
+    
+    if type(groupby) is not list:
+        groupby = [groupby]
+    
+    # collapse the dataframe based on keys
+    if key is not None:
+        if type(key) is not list:
+            key = [key]
+        df = df.drop_duplicates(key)
+    
+    # normal cumulative count hence disregards other values that should share position
+    df['temp1'] = df.sort_values(by=date, ascending=True).groupby(groupby).cumcount() + 1
+    
+    # counts duplicates 
+    temp = df.groupby(groupby+[date]).size().reset_index(name='temp2')
+
+    df = pd.merge(df, temp, on=groupby+[date])
+    
+    # take max of duplicate and normal cumulative count 
     df[name] = df[['temp1', 'temp2']].max(axis=1)
+    
     df = df.drop(columns=['temp1', 'temp2'])
     return df
 
