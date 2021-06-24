bff10937b0c25b5d811ac6f0e7a77a45af13d3d9
Kai Zhang <kzhang288@gmail.com>
2021-06-24 11:20:37 +0100

    Moving MLP to separate file

--- fedcox.py
+++ fedcox.py
@@ -4,50 +4,13 @@ import torch
 import torch.nn.functional as F
 
 from tensorboardX import SummaryWriter
-from torch import nn
 from torch import Tensor
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
 
 rng = np.random.default_rng(123)
 
-class DenseBlock(nn.Module):
-    def __init__(self, dim_in, dim_out, bias=True, batch_norm=True, dropout=0, activation=nn.ReLU, 
-                    w_init_=lambda w: nn.init.kaiming_normal_(w, nonlinearity='relu')):
-        super().__init__()
-        self.linear = nn.Linear(dim_in, dim_out, bias)
-        # fill in self.linear.weight.data with kaiming normal
-        if w_init_:
-            w_init_(self.linear.weight.data)
-        self.activation = activation()
-        self.batch_norm = nn.BatchNorm1d(dim_out) if batch_norm else None
-        self.dropout = nn.Dropout(dropout) if dropout else None
 
-    def forward(self, input):
-        input = self.activation(self.linear(input))
-        if self.batch_norm:
-            input = self.batch_norm(input)
-        if self.dropout:
-            input = self.dropout(input)
-        return input
-
-class MLP(nn.Module):
-    def __init__(self, dim_in, num_nodes, dim_out, batch_norm=True, dropout=None, activation=nn.ReLU,
-                output_activation=None, output_bias=True,
-                w_init_=lambda w: nn.init.kaiming_normal_(w, nonlinearity='relu')):
-        super().__init__()
-        nodes = [dim_in].append(num_nodes)
-        net = []
-        for d_in, d_out in zip(nodes[:-1], nodes[1:]):
-            net.append(DenseBlock(d_in, d_out, bias=True, batch_norm=batch_norm, 
-                        dropout=dropout, activation=activation, w_init_=w_init_))
-        net.append(nn.Linear(num_nodes[-1], dim_out, output_bias))
-        if output_activation:
-            net.append(output_activation)
-        self.net = nn.Sequential(*net)
-
-    def forward(self, input):
-        return self.net(input)
 
 # scale the loss to make it data size invariant
 def _reduction(loss: Tensor, reduction: str = 'mean') -> Tensor:
