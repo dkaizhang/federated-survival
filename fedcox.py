d365da33885e8028bb25cdfc2a9ab173de391fcd
Kai Zhang <kzhang288@gmail.com>
2021-06-24 11:25:42 +0100

    Moving loss function to separate file

--- fedcox.py
+++ fedcox.py
@@ -1,60 +1,14 @@
 import copy
 import numpy as np
 import torch
-import torch.nn.functional as F
 
 from dataset import DatasetSplit, sample_iid
+from loss import negative_llh
 
 from tensorboardX import SummaryWriter
-from torch import Tensor
 from torch.utils.data import DataLoader
-from torch.utils.data import TensorDataset
 
 rng = np.random.default_rng(123)
-
-
-
-# scale the loss to make it data size invariant
-def _reduction(loss: Tensor, reduction: str = 'mean') -> Tensor:
-    if reduction == 'none':
-        return loss
-    elif reduction == 'mean':
-        return loss.mean()
-    raise ValueError(f"`reduction` = {reduction} is not valid. Use 'none', or 'mean'.")
-
-def negative_llh(phi: Tensor, idx_durations: Tensor, events: Tensor, 
-                reduction: str = 'mean') -> Tensor:
-    """
-    Arguments:
-        phi -- N x T. Estimates in (-inf, inf) where hazard = sigmoid(phi). 
-        idx_durations -- N. Event times for each individual represented as indices based on discretisation.
-        events -- N. Indicator of event (1.) or censoring (0.) for each individual. Floats.
-    Returns:
-        Negative LLH as a 1dim Tensor
-    """
-    if phi.shape[1] <= idx_durations.max():
-        raise ValueError(f"Network output `phi` is too small for `idx_durations`."+
-                         f" Need at least `phi.shape[1] = {idx_durations.max().item()+1}`,"+
-                         f" but got `phi.shape[1] = {phi.shape[1]}`")
-    if events.dtype is torch.bool:
-        events = events.float()
-    # stack events and idx_durations to get Nx1 Tensor
-    # .view(-1, 1) -- infer first dimension to get second dimension as size=1 
-    events = events.view(-1, 1)    
-    idx_durations = idx_durations.view(-1, 1)
-    # create NxT zero Tensor and put in each row at the idx_duration whether the event occurred
-    # censored ppl have row of zeros, uncensored ppl have row with a single one
-    y_bce = torch.zeros_like(phi).scatter(1, idx_durations, events)
-    # with logits to pass phi through a sigmoid first -- recall it's unconstrained
-    bce = F.binary_cross_entropy_with_logits(phi, y_bce, reduction='none')
-    # sum the loss until failure time:
-        # first sum until each time
-        # then gather the summed loss at relevant failure time
-    loss = bce.cumsum(1).gather(1, idx_durations).view(-1)
-    return _reduction(loss, reduction)
-
-
-
 
 
 def average_weights(w):
@@ -70,10 +24,6 @@ def average_weights(w):
             w_avg[key] += w[i][key]
         w_avg[key] = torch.div(w_avg[key], len(w))
     return w_avg
-
-
-
-
 
 
 class Member():
---
+++ loss.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn.functional as F
+
+from torch import Tensor
+
+# scale the loss to make it data size invariant
+def _reduction(loss: Tensor, reduction: str = 'mean') -> Tensor:
+    if reduction == 'none':
+        return loss
+    elif reduction == 'mean':
+        return loss.mean()
+    raise ValueError(f"`reduction` = {reduction} is not valid. Use 'none', or 'mean'.")
+
+def negative_llh(phi: Tensor, idx_durations: Tensor, events: Tensor, 
+                reduction: str = 'mean') -> Tensor:
+    """
+    Arguments:
+        phi -- N x T. Estimates in (-inf, inf) where hazard = sigmoid(phi). 
+        idx_durations -- N. Event times for each individual represented as indices based on discretisation.
+        events -- N. Indicator of event (1.) or censoring (0.) for each individual. Floats.
+    Returns:
+        Negative LLH as a 1dim Tensor
+    """
+    if phi.shape[1] <= idx_durations.max():
+        raise ValueError(f"Network output `phi` is too small for `idx_durations`."+
+                         f" Need at least `phi.shape[1] = {idx_durations.max().item()+1}`,"+
+                         f" but got `phi.shape[1] = {phi.shape[1]}`")
+    if events.dtype is torch.bool:
+        events = events.float()
+    # stack events and idx_durations to get Nx1 Tensor
+    # .view(-1, 1) -- infer first dimension to get second dimension as size=1 
+    events = events.view(-1, 1)    
+    idx_durations = idx_durations.view(-1, 1)
+    # create NxT zero Tensor and put in each row at the idx_duration whether the event occurred
+    # censored ppl have row of zeros, uncensored ppl have row with a single one
+    y_bce = torch.zeros_like(phi).scatter(1, idx_durations, events)
+    # with logits to pass phi through a sigmoid first -- recall it's unconstrained
+    bce = F.binary_cross_entropy_with_logits(phi, y_bce, reduction='none')
+    # sum the loss until failure time:
+        # first sum until each time
+        # then gather the summed loss at relevant failure time
+    loss = bce.cumsum(1).gather(1, idx_durations).view(-1)
+    return _reduction(loss, reduction)
