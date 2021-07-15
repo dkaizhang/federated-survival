1be5300ce2b2c7e56d8c7039b5827f38baeb7315
Kai Zhang <kzhang288@gmail.com>
2021-07-15 19:40:24 +0100

    New test

---
+++ test_net.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pytest
+import torch
+
+from net import MLP
+
+@pytest.mark.parametrize('dim_out', [1,5,15])
+@pytest.mark.parametrize('seed', [0,2,4])
+def test_net_dimensions(dim_out, seed):
+    torch.manual_seed(seed)
+    N = 5
+    dim_in = 10 
+    num_nodes = [32, 32]
+    net = MLP(dim_in=dim_in, num_nodes=num_nodes, dim_out=dim_out)
+
+    input = torch.randn((N, dim_in))
+    output = net(input)
+    assert(output.shape[0] == N and output.shape[1] == dim_out)
