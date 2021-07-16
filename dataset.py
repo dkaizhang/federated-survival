2e5fe62770487674ab4ec1cf25b49a5a246ad028
Kai Zhang <kzhang288@gmail.com>
2021-07-16 11:48:16 +0100

    Shelved malignant benign; quantile sampler

--- dataset.py
+++ dataset.py
@@ -3,7 +3,7 @@ import torch
 
 rng = np.random.default_rng(123)
 
-def sample_iid(data, num_centers):
+def sample_iid(data, num_centers, start_center = 0):
     """
     Randomly split data evenly across each centre
     Arguments:
@@ -13,37 +13,71 @@ def sample_iid(data, num_centers):
     Dict with centre_id : indices of data assigned to centre
     """
     num_items = int(len(data)/num_centers)
+    # all_idxs doesn't look at the actual existing indices in data
     dict_center_idxs, all_idxs = {}, [i for i in range(len(data))]
     for i in range(num_centers):
-        dict_center_idxs[i] = set(rng.choice(all_idxs, num_items,
-                                             replace=False))
+        i = i + start_center
+
+        selected_idxs = list(rng.choice(all_idxs, num_items, replace=False))
+        
+        # rebase to the actual indices in data
+        dict_center_idxs[i] = set(data.iloc[selected_idxs].index)
         all_idxs = list(set(all_idxs) - dict_center_idxs[i])
     return dict_center_idxs    
 
 
-def sample_benign_malignant(data, num_centers):
+def sample_by_quantiles(data, column, num_centers):
     """
-    Split data between benign and malignant tumours then distribute across each centre
+    Randomly split data by age groups
     Arguments:
     data -- combined data
+    column -- column on which to stratify
     num_centres -- number of centres to spread over    
     Returns:
     Dict with centre_id : indices of data assigned to centre
     """
-    malignant = (data['SITE_C71'] == 1) | (data['SITE_C70'] == 1) | (data['SITE_C72'] == 1)
-    benign = (data['SITE_D32'] == 1) | (data['SITE_D33'] == 1) | (data['SITE_D35'] == 1)    
-    
-    malignant = data.loc[malignant]
-    benign = data.loc[benign]
-
-    malignant_dict = sample_iid(malignant, num_centers // 2)
-    benign_dict = sample_iid(benign, num_centers - num_centers // 2)
 
     dict_center_idxs = {}
-    dict_center_idxs.update(malignant_dict)
-    dict_center_idxs.update(benign_dict)
+    quantile = 1 / num_centers
+    previous_idxs = set()
+
+    for i in range(num_centers):
+        if quantile > 1:
+            ValueError
+        cutoff = data[column].quantile(quantile)
+        selected_idxs = data[column] <= cutoff 
+        idxs_in_quantile = set(data.loc[selected_idxs].index) - previous_idxs
+        previous_idxs = previous_idxs | idxs_in_quantile
+        dict_center_idxs[i] = idxs_in_quantile
+        quantile += 1 / num_centers 
 
     return dict_center_idxs    
+
+
+# need to deal with those who have both benign and malignant tumour
+# def sample_benign_malignant(data, num_centers):
+#     """
+#     Split data between benign and malignant tumours then distribute across each centre
+#     Arguments:
+#     data -- combined data
+#     num_centres -- number of centres to spread over    
+#     Returns:
+#     Dict with centre_id : indices of data assigned to centre
+#     """
+#     malignant = (data['SITE_C71'] == 1) | (data['SITE_C70'] == 1) | (data['SITE_C72'] == 1)
+#     benign = (data['SITE_D32'] == 1) | (data['SITE_D33'] == 1) | (data['SITE_D35'] == 1)    
+    
+#     malignant = data.loc[malignant]
+#     benign = data.loc[benign]
+
+#     malignant_dict = sample_iid(malignant, num_centers // 2)
+#     benign_dict = sample_iid(benign, num_centers - num_centers // 2, start_center = num_centers // 2)
+
+#     dict_center_idxs = {}
+#     dict_center_idxs.update(malignant_dict)
+#     dict_center_idxs.update(benign_dict)
+
+#     return dict_center_idxs    
 
 
 
--- test_dataset.py
+++ test_dataset.py
@@ -1,32 +1,88 @@
 import numpy as np
+from numpy.random import sample
 import pandas as pd
 import pytest
 import torch 
 
-from dataset import sample_iid, sample_benign_malignant
+from dataset import sample_iid, sample_by_quantiles
 
 @pytest.mark.parametrize('seed', [0,2,4])
 @pytest.mark.parametrize('centers', [2,5,10])
 def test_no_overlap_iid(seed,centers):
     N = 100
-    indicators = torch.randint(low=0, high=2, size=(N,2)).numpy()
-    data = pd.DataFrame(indicators, columns=['SITE_C71', 'SITE_D32'])
+    indicators = torch.randint(low=0, high=2, size=(N,6)).numpy()
+    data = pd.DataFrame(indicators, columns=['SITE_C71', 'SITE_C70', 'SITE_C72', 'SITE_D32', 'SITE_D33', 'SITE_D35'])
     center_idxs = sample_iid(data, centers)
     idxs = []
     for key in center_idxs:
         idxs = idxs + list(center_idxs[key])
+    assert(len(center_idxs.keys()) == centers)
     assert(len(idxs) == len(set(idxs))) 
-
 
 
 @pytest.mark.parametrize('seed', [0,2,4])
 @pytest.mark.parametrize('centers', [2,5,10])
-def test_no_overlap_noniid(seed,centers):
+@pytest.mark.parametrize('start_center', [0,1,2])
+def test_keys_iid(seed,centers, start_center):
     N = 100
     indicators = torch.randint(low=0, high=2, size=(N,6)).numpy()
     data = pd.DataFrame(indicators, columns=['SITE_C71', 'SITE_C70', 'SITE_C72', 'SITE_D32', 'SITE_D33', 'SITE_D35'])
-    center_idxs = sample_benign_malignant(data, centers)
+    center_idxs = sample_iid(data, centers, start_center=start_center)
+    for i, k in enumerate(center_idxs.keys()):
+        assert(int(k) == i + start_center)
+
+
+@pytest.mark.parametrize('seed', [0,2,4])
+@pytest.mark.parametrize('centers', [2,5,10])
+def test_keys_quantile(seed,centers):
+    N = 100
+    ages = torch.rand(size=(N,1)).numpy()
+    data = pd.DataFrame(ages, columns=['AGE'])
+    center_idxs = sample_by_quantiles(data, 'AGE', centers)
+    all_centers = [i for i in range(centers)]
+    for i, k in enumerate(center_idxs.keys()):
+        assert(int(k) == all_centers[i])
+    assert(len(center_idxs.keys()) == centers)
+
+@pytest.mark.parametrize('seed', [0,2,4])
+@pytest.mark.parametrize('centers', [2,5,10])
+def test_no_overlap_quantile(seed,centers):
+    N = 100
+    ages = torch.rand(size=(N,1)).numpy()
+    data = pd.DataFrame(ages, columns=['AGE'])
+    center_idxs = sample_by_quantiles(data, 'AGE', centers)
     idxs = []
+    prev_max = 0
     for key in center_idxs:
         idxs = idxs + list(center_idxs[key])
+        assert(data.loc[center_idxs[key]].to_numpy().max() > prev_max)
+        prev_max = data.loc[center_idxs[key]].to_numpy().max()
     assert(len(idxs) == len(set(idxs))) 
+    assert(len(idxs) == N) 
+    
+
+
+# @pytest.mark.parametrize('seed', [0,2,4])
+# @pytest.mark.parametrize('centers', [2,5,10])
+# def test_keys_benign_malignant(seed,centers):
+#     N = 100
+#     indicators = torch.randint(low=0, high=2, size=(N,6)).numpy()
+#     data = pd.DataFrame(indicators, columns=['SITE_C71', 'SITE_C70', 'SITE_C72', 'SITE_D32', 'SITE_D33', 'SITE_D35'])
+#     center_idxs = sample_benign_malignant(data, centers)
+#     all_centers = [i for i in range(centers)]
+#     for i, k in enumerate(center_idxs.keys()):
+#         assert(int(k) == all_centers[i])
+
+# overlap as some have both
+# @pytest.mark.parametrize('seed', [0,2,4])
+# @pytest.mark.parametrize('centers', [2,5,10])
+# def test_no_overlap_benign_malignant(seed,centers):
+#     N = 100
+#     indicators = torch.randint(low=0, high=2, size=(N,6)).numpy()
+#     data = pd.DataFrame(indicators, columns=['SITE_C71', 'SITE_C70', 'SITE_C72', 'SITE_D32', 'SITE_D33', 'SITE_D35'])
+#     center_idxs = sample_benign_malignant(data, centers)
+#     idxs = []
+#     for key in center_idxs:
+#         idxs = idxs + list(center_idxs[key])
+#     assert(len(center_idxs.keys()) == centers)
+#     assert(len(idxs) == len(set(idxs))) 
